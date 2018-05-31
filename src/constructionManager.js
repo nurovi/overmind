2c99ed7dcde8994a34f9b78d9463131194bf1958
Justin Robb <jrobb@meredith.com>
2018-05-31 14:50:02 -0700

    Added extension building

--- src/constructionManager.js
+++ src/constructionManager.js
@@ -9,23 +9,20 @@ const constructionManager = {
 
     if (myConstructionSites.length >= allowedConstructionSitesMaximum) {
       // Already busy building something, wait until it's done to build something else
-      console.log(`We are currently building ${myConstructionSites.length} structures in room ${room.name}: [${myConstructionSites.map(s => s.structureType).join(', ')}]`);
       return;
     }
 
     const myStructures = room.find(FIND_MY_STRUCTURES);
-    console.log(`Looking to build new structure(s). We currently have ${myStructures.length} structures in room ${room.name}: [${myStructures.map(s => s.structureType).join(', ')}]`);
+    const spawn = myStructures.filter(s => s.structureType === STRUCTURE_SPAWN)[0];
 
     // Prioritize building a container near the closest mining site
     if (controllerLevel > 0 && myStructures.filter(s => s.structureType === STRUCTURE_CONTAINER).length === 0) {
       // Find closest mining site
-      const spawn = myStructures.filter(s => s.structureType === STRUCTURE_SPAWN)[0];
       const activeSources = room.find(FIND_SOURCES_ACTIVE);
 
       const closestSource = activeSources.sort((a, b) => {
         const distanceA = Math.sqrt(((spawn.pos.x - a.pos.x) ** 2) + ((spawn.pos.y - a.pos.y) ** 2));
         const distanceB = Math.sqrt(((spawn.pos.x - b.pos.x) ** 2) + ((spawn.pos.y - b.pos.y) ** 2));
-
         return distanceA - distanceB;
       })[0];
 
@@ -39,6 +36,41 @@ const constructionManager = {
         console.log(`Success placing ${STRUCTURE_CONTAINER} in room ${room.name} at pos (${structurePos.x}, ${structurePos.y})`);
       } else {
         console.log(`Error (${err}) placing ${STRUCTURE_CONTAINER} in room ${room.name} at pos (${structurePos.x}, ${structurePos.y})`);
+      }
+      return;
+    }
+
+    // Next build extensions close to the spawn
+    let possibleExtensions = controllerLevel < 2 ? 0 : 5;
+    possibleExtensions = controllerLevel <= 2 ? controllerLevel : (controllerLevel - 2) * 10;
+    const builtExtensions = myStructures.filter(s => s.structureType === STRUCTURE_EXTENSION).length;
+    const availableExtensions = possibleExtensions - builtExtensions;
+    if (availableExtensions > 0) {
+      // We want to build extensions!
+      // find the closest free space to the spawn using diagonal jumps of two
+      const isFree = pos => Game.map.getTerrainAt(pos) !== 'wall' && room.lookAt(pos).filter(a => a.type !== LOOK_TERRAIN && a.type !== LOOK_CREEPS).length === 0;
+      let firstFreePos;
+      for (let i = 2; i < 50; i += 2) { // rooms ar all 50x50
+        for (let j = 0; i < 4; j += 1) { // diagonals
+          const xDir = j < 2 ? -1 : 1;
+          const yDir = j % 2 === 0 ? -1 : 1;
+          const x = spawn.pos.x + (xDir * i);
+          const y = spawn.pos.y + (yDir * i);
+          const pos = room.getPositionAt(x, y);
+          if (isFree(pos)) {
+            firstFreePos = pos;
+            break;
+          }
+        }
+      }
+
+      if (firstFreePos) {
+        const err = room.createConstructionSite(firstFreePos, STRUCTURE_EXTENSION);
+        if (err === 0) {
+          console.log(`success placing ${STRUCTURE_EXTENSION} in room ${room.name} at pos (${firstFreePos.x}, ${firstFreePos.y})`);
+        } else {
+          console.log(`error (${err}) placing ${STRUCTURE_EXTENSION} in room ${room.name} at pos (${firstFreePos.x}, ${firstFreePos.y})`);
+        }
       }
     }
   }
